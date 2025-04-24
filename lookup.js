04edeac0764910b971ddc3d5c3f2c6156bfc6b28
Wulfsnarm <jmprather97@gmail.com>
2025-04-23 21:07:19 -0500

    updated init_settings to now check if the user has settings already, if not, it will create the settings

--- lookup.js
+++ lookup.js
@@ -10,6 +10,7 @@ function search(){
 	
 	//console.log(search_string);
 
+    //if search_string empty return full data set
     if (search_string == ""){
         for (book in data){
 			for (chapter in data[book]){
@@ -19,155 +20,197 @@ function search(){
                 }
             }
         }
-    }
-	
+    }else{
+        for (var x = 0; x < search_string.length; x++){
+            extra = (search_string[x].match(/[+]/g) || []).length;
+            vers_count = -1;
+            //iterate through each verse in data set
+            for (book in data){
+                for (chapter in data[book]){
+                    for (verse in data[book][chapter]){
+                        vers_count++;
 
-	for (var x = 0; x < search_string.length; x++){
-		for (book in data){
-			for (chapter in data[book]){
-				for (verse in data[book][chapter]){
-					var txt = data[book][chapter][verse];
-					var match = true;
-					var book_match = false;
-					var chap_match = false;
-					var vers_match = false;
-                    console.log("search_string:");
-                    console.log(search_string);
-                    console.log("x: " + x);
-					var search_terms = search_string[x].split(" ");
-					
-					//console.log(search_terms.length);
-					if (!(search_terms.length == 1 && search_terms[0] == "")){
-						for (var y = 0; y < search_terms.length; y++){
-							var search = search_terms[y];
-							
-							//console.log("search: " + search);
-							if (search != "" && match){
-								if (txt.search(RegExp("("+search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")", "gi")) >= 0){
-									match = true;
-									//console.log("found");
-								}else if (search.search(RegExp("([A-z]{1,20}[0-9]{1,3}\.[0-9]{1,2})", "gi")) >= 0){
-									var search_book = search.slice(search.search(/[A-z]/), search.search(/[0-9]/));
-									var search_chap = search.slice(search.search(/[0-9]/), search.search(/\./));
-									var search_vers = search.slice(search.search(/\./) + 1, search.length);
-									
-									//console.log("Book " + book);
-									//console.log("Chap " + search_chap);
-									//console.log("Vers " + search_vers);
-									
-									if (book.search(RegExp("("+search_book.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")", "gi")) >= 0 && chapter == search_chap && verse == search_vers){
-										match = true;
-										vers_match = true;
-									} else {
-										match = false;
-									}
-									//console.log("reference");
-								}else if (search.search(RegExp("([A-z]{1,20}[0-9]{1,3})", "gi")) >= 0){
-									var search_book = search.slice(search.search(/[A-z]/), search.search(/[0-9]/));
-									var search_chap = search.slice(search.search(/[0-9]/), search.length);
-									
-									//console.log("Book " + book);
-									//console.log("Chap " + search_chap);
-									
-									if (book.search(RegExp("("+search_book.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")", "gi")) >= 0 && chapter == search_chap){
-										match = true;
-										chap_match = true;
-									} else {
-										match = false;
-									}
-									//console.log("reference");
-								}else if (book.search(RegExp("("+search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")", "gi")) >= 0){
-										match = true;
-										book_match = true;
-									//console.log("reference");
-								}else{
-									match = false;
-									//console.log("no match");
-								}
-							}
-						}
-						
-						var highlight = "";
-						
-						if (match)	{		
-							highlight_arr = txt.split(/[<>]+/);
-						
-							//console.log("highlighting");
-							
-							regex_string = "";
-							
-							replace_obj = {};
-						
-							for (var y = 0; y < search_terms.length; y++){
-								var search = search_terms[y];
-								
-								//console.log(search_terms.length);
-								
-								if (regex_string == "")
-									regex_string = "("+search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")";
-								else
-									regex_string = regex_string + "|("+search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")";
-							}
-							
-							var regex = new RegExp(regex_string, "gi");
-							
-							console.log(regex);
-                            
-                            highlight = "";
-                            for (h = 0; h < highlight_arr.length; h++){
-                                if (!highlight_arr[h].includes("span")){
-                                    highlight = highlight + highlight_arr[h].replace(regex, function (matched) {
-                                        return "<span class='highlight'>"+matched+"</span>";
-                                    });
-                                }else{
-                                    highlight = highlight + "<" + highlight_arr[h] + ">"
+                        var txt = data[book][chapter][verse];
+                        var match = true;
+                        var book_match = false;
+                        var chap_match = false;
+                        var vers_match = false;
+                        //console.log("search_string:");
+                        //console.log(search_string);
+                        //console.log("x: " + x);
+                        var search_terms = search_string[x].split(" ");
+                        
+                        //iterate through individual search terms as long as they are not empty
+                        //console.log(search_terms.length);
+                        if (!(search_terms.length == 1 && search_terms[0] == "")){
+                            pre_extra = [];
+                            post_extra = [];
+
+                            for (var y = 0; y < search_terms.length; y++){
+                                var search = search_terms[y];
+                                
+                                //console.log("search: " + search);
+                                if (search != "" && match){
+                                    //search data string but remove any weird characters in the search string
+                                    if (txt.search(RegExp("("+search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '')+")", "gi")) >= 0){
+                                        match = true;
+                                        //console.log("found");
+                                    //search data string for specific book chapter and verse
+                                    }else if (search.search(RegExp("([A-z]{1,20}[0-9]{1,3}\.[0-9]{0,2})", "gi")) >= 0){
+                                        var search_book = search.slice(search.search(/[A-z]/), search.search(/[0-9]/));
+                                        var search_chap = search.slice(search.search(/[0-9]/), search.search(/\./));
+                                        var search_vers = search.slice(search.search(/\./) + 1, search.length);
+                                        
+                                        //console.log("Book(vers) " + book);
+                                        //console.log("Chap(vers) " + search_chap);
+                                        //console.log("Vers(vers) " + search_vers);
+                                        
+                                        if (book.search(RegExp("("+search_book.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")", "gi")) >= 0 && chapter.includes(search_chap) && verse.includes(search_vers)){
+                                            match = true;
+                                            vers_match = true;
+                                        } else {
+                                            match = false;
+                                        }
+                                        //console.log("reference");
+                                    }else if (search.search(RegExp("([A-z]{1,20}[0-9]{1,3})", "gi")) >= 0){
+                                        var search_book = search.slice(search.search(/[A-z]/), search.search(/[0-9]/));
+                                        var search_chap = search.slice(search.search(/[0-9]/), search.length);
+                                        
+                                        //console.log("Book(chap) " + search_book);
+                                        //console.log("Chap(chap) " + search_chap);
+                                        
+                                        if (book.search(RegExp("("+search_book.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")", "gi")) >= 0 && chapter.includes(search_chap)){
+                                            match = true;
+                                            chap_match = true;
+                                        } else {
+                                            match = false;
+                                        }
+                                        //console.log("reference");
+                                    }else if (book.search(RegExp("("+search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')+")", "gi")) >= 0){
+                                            match = true;
+                                            book_match = true;
+                                        //console.log("reference");
+                                    }else{
+                                        match = false;
+                                        //console.log("no match");
+                                    }
                                 }
                             }
 
-							//console.log(highlight);
-							
-							if (!book_match && !chap_match && !vers_match){
-								result_string = result_string + "<b>" + book + " " + chapter + ":" + verse + "</b> " + highlight + "\n\n";
-							} else if (book_match) {
-								result_string = result_string + "<b><span class='highlight'>" + book + "</span> " + chapter + ":" + verse + "</b> " + highlight + "\n\n";
-							} else if (chap_match) {
-								result_string = result_string + "<b><span class='highlight'>" + book + " " + chapter + "</span>:" + verse + "</b> " + highlight + "\n\n";
-							} else if (vers_match) {
-								result_string = result_string + "<b><span class='highlight'>" + book + " " + chapter + ":" + verse + "</span></b> " + highlight + "\n\n";
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+                            var highlight = "";
+                            
+                            if (match)	{		
+                                highlight_arr = txt.split(/[<>]+/);
+                            
+                                //console.log("highlighting");
+                                
+                                regex_string = "";
+                                
+                                replace_obj = {};
+                            
+                                for (var y = 0; y < search_terms.length; y++){
+                                    var search = search_terms[y];
+                                    
+                                    //console.log(search_terms.length);
+                                    
+                                    if (regex_string == "")
+                                        regex_string = "("+search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '')+")";
+                                    else
+                                        regex_string = regex_string + "|("+search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '')+")";
+                                }
+                                
+                                var regex = new RegExp(regex_string, "gi");
+                                
+                                console.log(regex);
+                                
+                                highlight = "";
+                                for (h = 0; h < highlight_arr.length; h++){
+                                    if (!highlight_arr[h].includes("span")){
+                                        highlight = highlight + highlight_arr[h].replace(regex, function (matched) {
+                                            return "<span class='highlight'>"+matched+"</span>";
+                                        });
+                                    }else{
+                                        highlight = highlight + "<" + highlight_arr[h] + ">"
+                                    }
+                                }
+    
+                                //console.log(highlight);
+                                
+                                if (!book_match && !chap_match && !vers_match){
+                                    pre_n_post = extra_verses(extra, data, book, chapter, verse, result_string);
+                                    result_string = result_string + pre_n_post[0] + "<b>" + book + " " + chapter + ":" + verse + "</b> " + highlight + "\n" + pre_n_post[1] + "\n";
+                                } else if (book_match) {
+                                    pre_n_post = extra_verses(extra, data, book, chapter, verse, result_string);
+                                    result_string = result_string + pre_n_post[0] + "<b><span class='highlight'>" + book + "</span> " + chapter + ":" + verse + "</b> " + highlight + "\n" + pre_n_post[1] + "\n";
+                                } else if (chap_match) {
+                                    pre_n_post = extra_verses(extra, data, book, chapter, verse, result_string);
+                                    result_string = result_string + pre_n_post[0] + "<b><span class='highlight'>" + book + " " + chapter + "</span>:" + verse + "</b> " + highlight + "\n" + pre_n_post[1] + "\n";
+                                } else if (vers_match) {
+                                    pre_n_post = extra_verses(extra, data, book, chapter, verse, result_string);
+                                    result_string = result_string + pre_n_post[0] + "<b><span class='highlight'>" + book + " " + chapter + ":" + verse + "</span></b> " + highlight + "\n" + pre_n_post[1] + "\n";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 	$("#results_box").html(result_string);
     searchOptions();
 }
 
+function extra_verses(count, data, book, chap, vers, result_string){
+    pre_result = "";
+    post_result = "";
+    verse_list = Object.keys(data[book][chap])
+    if (count > 0){
+        for (ext_i = 1; ext_i <= count; ext_i++){
+            if (verse_list.indexOf(vers)-ext_i >= 0){
+                pre_verse = verse_list[verse_list.indexOf(vers)-ext_i]
+                pre_result = "<b>" + book + " " + chap + ":" + pre_verse + "</b> " + data[book][chap][pre_verse] + "\n" + pre_result;
+            }
+            if (verse_list.indexOf(vers)+ext_i < verse_list.length){
+                post_verse = verse_list[verse_list.indexOf(vers)+ext_i]
+                post_result = post_result + "<b>" + book + " " + chap + ":" + post_verse + "</b> " + data[book][chap][post_verse] + "\n";
+            }
+        }
+    }
+    return [pre_result, post_result];
+}
+
 function init_settings(){	
 	
-	var settings = localStorage.getItem('settings').split(",");
-	
-	//set division based on storage
-	for (x = 0; x < document.getElementById("division").children.length; x ++){
-		(parseInt(settings[2]) == x) ? document.getElementById("division").children[x].selected = true : document.getElementById("division").children[x].selected = false;
-	}
+    //check if stored settings exist
+	if (localStorage.getItem('settings')){
+        //set division based on storage
+        settings = localStorage.getItem('settings').split(",")
+        for (x = 0; x < document.getElementById("division").children.length; x ++){
+            (parseInt(settings[2]) == x) ? document.getElementById("division").children[x].selected = true : document.getElementById("division").children[x].selected = false;
+        }
+    }else{
+        for (x = 0; x < document.getElementById("division").children.length; x ++){
+            (parseInt(0) == x) ? document.getElementById("division").children[x].selected = true : document.getElementById("division").children[x].selected = false;
+        }
+    }
 }
 
 function set_settings(){
 	
-    var settings = localStorage.getItem('settings').split(",");
+    if (localStorage.getItem('settings')){
+        //set division based on storage
+        settings = localStorage.getItem('settings').split(",")
 
-	var redTeam = parseInt(settings[0]);
-	
-	var yellowTeam = parseInt(settings[1]);
-	
-	var division = $("select[id='division'] option:selected").index();
-	
-	var tenpointRed = parseInt(settings[3]);
-	
-	var tenpointYellow = parseInt(settings[4]);
+        var redTeam = parseInt(settings[0]);
+        
+        var yellowTeam = parseInt(settings[1]);
+        
+        var division = $("select[id='division'] option:selected").index();
+        
+        var tenpointRed = parseInt(settings[3]);
+        
+        var tenpointYellow = parseInt(settings[4]);
+    }
 	
 	localStorage.setItem('settings', redTeam + "," + yellowTeam + "," + division + "," + tenpointRed + "," + tenpointYellow);
 	
@@ -240,6 +283,17 @@ function searchOptions(){
     }
 }
 
+function debounce(callback, ms) {
+    var timer = 0;
+    return function() {
+        var context = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            callback.apply(context, args);
+        }, ms || 0);
+    };
+}
+
 $(document).ready(function(){
 
     init_settings();
@@ -249,5 +303,5 @@ $(document).ready(function(){
     search();
 
 	//////////// TEXT SEARCH FUNCTION ////////////////////
-	$("#search_field").keyup(function(){search();});
+	$("#search_field").keyup(debounce(function(e){search();}, 300));
 });
